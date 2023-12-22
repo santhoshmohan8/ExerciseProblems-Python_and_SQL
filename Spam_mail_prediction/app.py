38d8d4827abbaade4f21e13d015bb4d26354e43e
Santhosh Mohan <santhoshkumar.mhn@gmail.com>
2023-12-23 00:56:55 +0530

    updates

--- Spam_mail_prediction/app.py
+++ Spam_mail_prediction/app.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
 import pickle
+import sklearn
+
 
 model = pickle.load(open('model.pkl','rb'))
-feature_transform = pickle.load(open('feature_transform.pkl','rb'))
+feature_transform = pickle.load(open('vector.pkl','rb'))
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Routing Service is Healthy!! Second check"}
+    return {"message": "Routing Service is Healthy!! fourth check"}
+
+@app.get("/predict")
+async def predict():
+    input = "You won $1000, click submit in your email inline to claim the reward "
+    X = []
+    X.append(input)
+
+    data = feature_transform.transform(X)
+    prediction = model.predict(data)
+    output = ['Spam' if prediction == 1 else 'Ham']
+    return(output)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
