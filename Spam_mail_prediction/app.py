8053f443a634eb700c9f0b69fd7049fb449a297e
Santhosh Mohan <santhoshkumar.mhn@gmail.com>
2023-12-25 12:07:34 +0530

    updates

--- Spam_mail_prediction/app.py
+++ Spam_mail_prediction/app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 import pickle
 import sklearn
-
+import json
+import os
 
 model = pickle.load(open('model.pkl','rb'))
 feature_transform = pickle.load(open('vector.pkl','rb'))
@@ -9,10 +10,10 @@ feature_transform = pickle.load(open('vector.pkl','rb'))
 app = FastAPI()
 
 @app.get("/")
-def root():
+async def root():
     return {"message": "Routing Service is Healthy!! fourth check"}
 
-@app.get("/predict")
+@app.get ("/predict")
 async def predict():
     input = "You won $1000, click submit in your email inline to claim the reward "
     X = []
@@ -23,5 +24,11 @@ async def predict():
     output = ['Spam' if prediction == 1 else 'Ham']
     return(output)
 
+# @app.get("/predict_json")
+# async def predict_json():
+
+
 # if __name__ == "__main__":
 #     app.run(debug=True)
+
+# python3 -m uvicorn app:app --reload
