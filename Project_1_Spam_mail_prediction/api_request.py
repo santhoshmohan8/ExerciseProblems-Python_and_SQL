b566ee46f33a82920b5c2bbc8c43e4aedd6111e2
Santhosh Mohan <santhoshkumar.mhn@gmail.com>
2023-12-28 15:38:41 +0530

    updates

--- Project_1_Spam_mail_prediction/api_request.py
+++ Project_1_Spam_mail_prediction/api_request.py
@@ -3,8 +3,7 @@ import requests
 
 input = {
     'Message':'You won $1000, click submit in your email inline to claim the reward'
-    #'Please find attached PayPal offer letter, Review and acknowledge']
     }
 print(input)
-r = requests.get("http://127.0.0.1:8000/predict_json_2/", data=json.dumps(input))
+r = requests.get("http://127.0.0.1:8000/predict_json/", data=json.dumps(input))
 print(r.text)
--- Project_1_Spam_mail_prediction/app.py
+++ Project_1_Spam_mail_prediction/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, templating, Form, Body, UploadFile, File
+from fastapi import FastAPI, Request, templating, Form, UploadFile, File
 from fastapi.responses import HTMLResponse
 import pandas as pd
 import numpy as np
