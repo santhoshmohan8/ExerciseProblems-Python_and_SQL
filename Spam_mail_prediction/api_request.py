d0303da374e9d090a0e731af6a1c84cd31b612d5
Santhosh Mohan <santhoshkumar.mhn@gmail.com>
2024-01-09 13:52:09 +0530

    updates

--- Spam_mail_prediction/api_request.py
+++ Spam_mail_prediction/api_request.py
@@ -5,5 +5,8 @@ input = {
     'Message':'You won $1000, click submit in your email inline to claim the reward'
     }
 print(input)
-r = requests.get("http://127.0.0.1:8000/predict_json/", data=json.dumps(input))
+r = requests.post("http://127.0.0.1:8000/predict_json/", data=json.dumps(input))
 print(r.text)
+
+
+# curl -X POST -H "content_type:application/json" -d "{'Message':'You won $1000, click submit in your email inline to claim the reward'}" http://127.0.0.1:8000/predict_json/
--- Spam_mail_prediction/app.py
+++ Spam_mail_prediction/app.py
@@ -33,17 +33,33 @@ class values(BaseModel):
 async def root(req : Request):
     return template.TemplateResponse(name='index.html', context={'request':req})
 
-# output page
+# Submit message in form return output in another template
 @app.post("/predict_webpage") # DONE
 async def predict_webpage(req : Request, Email: str = Form(...)):
-    input = []
+    # print(type(Email)) # str
+    input = [] # want to convert str to list # you cant do directly, as it will create list by indivdual letters
     input.append(Email)
     data = feature_transform.transform(input)
+    # print(model.predict(data)[0]) # list output, making [0] returns int
     prediction = model.predict(data)[0]
     # return f'Prediction : {prediction} message'
     return template.TemplateResponse(name='output.html', context={'request':req, 'Prediction':prediction})
 
-@app.get ("/predict_text") # DONE
+# upload messages in form, return output as html
+@app.post ("/predict_file") # DONE
+async def pred_json(req: Request, file : UploadFile = File(...)):
+    file_content = file.file.read().decode('utf-8')
+    # print(type(file_content)) # str
+    msg = file_content.splitlines()
+    # print(type(msg)) # list
+
+    data = feature_transform.transform(msg)
+    prediction = model.predict(data)
+    output = ['Spam Message' if item == 1 else 'Ham Message' for item in prediction]
+    df = pd.DataFrame(msg,output).reset_index(drop=False).rename(columns={'index':'Prediction',0:'Message'})
+    return HTMLResponse(df.to_html())
+
+@app.post ("/predict_text") # DONE
 async def predict_text():
     input = "You won $1000, click submit in your email inline to claim the reward "
     X = []
@@ -53,10 +69,13 @@ async def predict_text():
     output = ['Spam Message' if prediction == 1 else 'Ham Message']
     return(output)
 
-@app.get ("/predict_json") # DONE
-async def pred_json(input_parameters : values):
-    input = input_parameters.json()
-    input = json.loads(input)
+@app.post ("/predict_json") # DONE
+async def pred_json(req:Request,input_parameters : values):
+    print(type(input_parameters)) # app values
+    input_1 = input_parameters.json()
+    # print(type(input_1)) # str - JSON format type will be represented as str
+    input = json.loads(input_1)
+    # print(type(input)) # dict
     X=[]
     X.append(input['Message'])
     data = feature_transform.transform(X)
@@ -64,16 +83,8 @@ async def pred_json(input_parameters : values):
     output = ['Spam Message' if prediction == 1 else 'Ham Message']
     return(output)
 
-@app.post ("/predict_file") # DONE
-async def pred_json(file : UploadFile = File(...)):
-    file_content = file.file.read().decode('utf-8')
-    msg = file_content.splitlines()
 
-    data = feature_transform.transform(msg)
-    prediction = model.predict(data)
-    output = ['Spam Message' if item == 1 else 'Ham Message' for item in prediction]
-    df = pd.DataFrame(msg,output).reset_index(drop=False).rename(columns={'index':'Prediction',0:'Message'})
-    return HTMLResponse(df.to_html())
+
 
 
 # if __name__ == "__main__":
